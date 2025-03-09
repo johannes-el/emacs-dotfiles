634996eee0f6c85ff401d3dfc64ba1d17a2a7419
johannese <Johannes.Elsing@gmx.de>
2025-03-09 10:02:09 +0100

    add sly and paredit

--- config.el
+++ config.el
@@ -19,10 +19,7 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package gruber-darker-theme
-:ensure t)
-:config
-  (load-theme 'gruber-darker)
+(load-theme 'modus-vivendi)
 
 (use-package evil
   :ensure t)
@@ -34,13 +31,30 @@
 (use-package eglot
   :ensure t)
 (require 'eglot)
+
+(add-hook 'lisp-mode-hook 'eglot-ensure)
+(add-hook 'lisp-interaction-mode-hook 'eglot-ensure)
+(add-hook 'emacs-lisp-mode-hook 'eglot-ensure)
+(add-hook 'python-mode-hook 'eglot-ensure)
+(add-hook 'rust-mode-hook 'eglot-ensure)
+(add-hook 'go-mode-hook 'eglot-ensure)
+
 (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
 (add-to-list 'eglot-server-programs '(rust-mode "rust-analyzer"))
-(add-hook 'c-mode-hook 'eglot-ensure)
-(add-hook 'c++-mode-hook 'eglot-ensure)
-(add-hook 'cmake-mode-hook 'eglot-ensure)
-(add-hook 'go-mode-hook 'eglot-ensure)
-(add-hook 'rust-mode-hook 'eglot-ensure)
+(add-to-list 'eglot-server-programs '((emacs-lisp-mode) . ("emacs-lsp")))
+(add-to-list 'eglot-server-programs '((lisp-mode lisp-interaction-mode) . ("cl-lsp")))
+
+(use-package sly
+  :ensure t)
+
+(use-package paredit
+  :ensure t
+  :hook ((lisp-mode . paredit-mode)
+         (emacs-lisp-mode . paredit-mode)
+         (clojure-mode . paredit-mode)
+         (scheme-mode . paredit-mode)
+         (sly-mode . paredit-mode)
+         (sly-repl-mode . paredit-mode)))
 
 (use-package magit
   :ensure t)
@@ -53,6 +67,8 @@
 (use-package projectile
   :ensure t)
 (setq projectile-mode +1)
+(global-set-key (kbd "C-c p f") 'projectile-find-file)
+(global-set-key (kbd "C-c p p") 'projectile-switch-project)
 
 (unless (file-exists-p "~/RoamNotes")
   (make-directory "~/RoamNotes"))
@@ -62,6 +78,17 @@
     (org-roam-directory "~/RoamNotes")
     :config
     (org-roam-setup))
+
+(use-package org-superstar
+  :ensure t
+  :hook (org-mode . org-superstar-mode)
+  :config
+  (setq org-superstar-headline-bullets-list '("•" "◉" "○" "◇" "◆"))
+  (setq org-superstar-todo-bullet-alist '(("TODO" . "◯")
+                                         ("DONE" . "✔")))
+  (setq org-superstar-item-bullet-alist '(("-" . "◆")
+                                         ("+" . "◇")
+                                         ("*" . "●"))))
 
 (use-package emms
   :ensure t)
